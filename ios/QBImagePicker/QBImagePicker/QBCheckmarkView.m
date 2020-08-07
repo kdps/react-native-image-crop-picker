ce59ee90e62bbbe81b0ac5c0fef274fcbafba062
김상훈 <shkimo@kodc.co.kr>
2020-08-07 17:56:32 +0900

    내부 색상 변경

--- ios/QBImagePicker/QBImagePicker/QBCheckmarkView.m
+++ ios/QBImagePicker/QBImagePicker/QBCheckmarkView.m
@@ -21,7 +21,7 @@
     self.checkmarkLineWidth = 1.2;
     
     self.borderColor = [UIColor whiteColor];
-    self.bodyColor = [UIColor colorWithRed:(20.0 / 255.0) green:(111.0 / 255.0) blue:(223.0 / 255.0) alpha:1.0];
+    self.bodyColor = [UIColor colorWithRed:(223.0 / 255.0) green:(20.0 / 255.0) blue:(20.0 / 255.0) alpha:1.0];
     self.checkmarkColor = [UIColor whiteColor];
     
     // Set shadow
