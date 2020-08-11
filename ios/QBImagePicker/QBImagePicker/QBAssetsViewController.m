370a90ef6fd3b09d3101c7f9b77c29b7166fa868
김상훈 <shkimo@kodc.co.kr>
2020-08-11 14:18:50 +0900

    55

--- ios/QBImagePicker/QBImagePicker/QBAssetsViewController.m
+++ ios/QBImagePicker/QBImagePicker/QBAssetsViewController.m
@@ -473,6 +473,19 @@ static int labelIndex = 0;
     cell.tag = indexPath.item;
     cell.showsOverlayViewWhenSelected = self.imagePickerController.allowsMultipleSelection;
 
+    cell.titleLabel.text = @"893";
+    [cell.titleLabel setText:@"123"];
+    
+    UILabel *label = (UILabel *)[cell viewWithTag:1];
+    //label = [[UILabel alloc] init];
+    CGSize size = [self collectionView:collectionView layout:collectionView.collectionViewLayout sizeForItemAtIndexPath:indexPath];
+    label.frame = CGRectMake(0, 0, size.width, size.height);
+    NSString *title = [[NSString alloc] initWithFormat:@"%@", @"tete"];
+    label.text = title;
+    
+    
+    
+    
     // Image
     PHAsset *asset = self.fetchResult[indexPath.item];
     CGSize itemSize = [(UICollectionViewFlowLayout *)collectionView.collectionViewLayout itemSize];
@@ -516,23 +529,8 @@ static int labelIndex = 0;
         //labelText = [NSString stringWithFormat:@"%d", labelIndex];
         
         /* Checkbox related function */
-        cell.titleLabel.text = @"893";
+        
         //cell.titleLabel.hidden = YES;
-        
-        
-        UILabel *label = (UILabel *)[cell viewWithTag:1];
-        //label = [[UILabel alloc] init];
-        CGSize size = [self collectionView:collectionView layout:collectionView.collectionViewLayout sizeForItemAtIndexPath:indexPath];
-        label.frame = CGRectMake(0, 0, size.width, size.height);
-        NSString *title = [[NSString alloc] initWithFormat:@"%@", @"tete"];
-        label.text = title;
-        
-        
-        
-        
-        
-        
-        
         
         [cell setSelected:YES];
         
